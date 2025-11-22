e46ea53785ab42e6f086822da068952d51282325
johnnycodes69 <sendmailtojohnny@gmail.com>
2025-11-22 08:56:43 -0500

    Update Palmer_Penguins_Body_Mass_vs_Flipper_Length.R
    New charts

--- Palmer_Penguins_Body_Mass_vs_Flipper_Length.R
+++ Palmer_Penguins_Body_Mass_vs_Flipper_Length.R
@@ -1,7 +1,37 @@
+install.packages('tidyverse')
+library(tidyverse)
+
 install.packages('palmerpenguins')
 library('palmerpenguins')
 
 install.packages('ggplot2')
 library('ggplot2')
 
-ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g)) + geom_point(aes(shape=species,color=species))+facet_wrap(~species) + labs(title="Palmer Penguins: Body Mass vs. Flipper Length")
+data("penguins")
+View(penguins)
+
+# visualize body mass vs flipper length (do more massive birds have larger flippers?)
+ggplot(data = penguins) + geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g))
+
+# visualize body mass vs flipper length segmented by species
+ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g)) +
+  geom_point(aes(shape=species,color=species)) + 
+  facet_wrap(~species) +
+  labs(title="Palmer Penguins: Body Mass vs. Flipper Length (Species)")
+
+# visualize body mass vs flipper length segmented by island 
+ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g)) +
+  geom_point(aes(shape=species,color=island)) + 
+  facet_wrap(~island) +
+  labs(title="Palmer Penguins: Body Mass vs. Flipper Length (Island)")
+
+
+# visualize body mass and island location (do bigger birds live on a particular island?)
+ggplot(penguins, aes(x = island, y = body_mass_g)) +
+  geom_boxplot() +
+  facet_wrap(~species) +
+  labs(
+    title = "Penguin Body Mass by Island",
+    x = "Island",
+    y = "Body Mass (g)"
+  )
