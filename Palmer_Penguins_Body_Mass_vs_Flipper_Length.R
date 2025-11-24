7bca6f5e8eeee752d438b31ff62d403f99d6f2d3
johnnycodes69 <sendmailtojohnny@gmail.com>
2025-11-24 08:44:11 -0500

    Improved charts with subtitles and annotations etc

--- Palmer_Penguins_Body_Mass_vs_Flipper_Length.R
+++ Palmer_Penguins_Body_Mass_vs_Flipper_Length.R
@@ -4,9 +4,6 @@ library(tidyverse)
 install.packages('palmerpenguins')
 library('palmerpenguins')
 
-install.packages('ggplot2')
-library('ggplot2')
-
 data("penguins")
 View(penguins)
 
@@ -17,7 +14,8 @@ ggplot(data = penguins) +
 # visualize as above but jitter instead of point due to overlap and smooth for trendline and B&W for readability
 ggplot(data = penguins) +
   geom_smooth(method = "gam", mapping = aes(x = flipper_length_mm, y = body_mass_g)) +
-  geom_jitter(mapping = aes(x = flipper_length_mm, y = body_mass_g, shape = species))
+  geom_jitter(mapping = aes(x = flipper_length_mm, y = body_mass_g, shape = species)) +
+  labs(title="Palmer Penguins Body Mass vs Flipper Length", subtitle="Measurements of Three Penguin Species")
 
 
 # visualize body mass vs flipper length segmented by species
@@ -30,7 +28,7 @@ ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g)) +
 ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g)) +
   geom_point(aes(shape=species,color=island)) + 
   facet_wrap(~island) +
-  labs(title="Palmer Penguins: Body Mass vs. Flipper Length (Island)")
+  labs(title="Palmer Penguins: Body Mass vs. Flipper Length (Island)", subtitle="Segmented by Habitat Island")
 
 
 # visualize body mass and island location (do bigger birds live on a particular island?)
@@ -39,11 +37,13 @@ ggplot(penguins, aes(x = island, y = body_mass_g, fill = island)) +
   facet_wrap(~species) +
   labs(
     title = "Penguin Body Mass by Island",
+    subtitle = "Mass by Habitat Island Segmented by Species",
     x = "Island",
-    y = "Body Mass (g)"
-  )
+    y = "Body Mass (g)")
 
 # visualize body mass and flipper length and facet on species, include sex of penguins if known
 ggplot(data=penguins) + 
   geom_point(mapping = aes(x = flipper_length_mm, y=body_mass_g, color=sex)) +
-  facet_grid(sex~species)
+  facet_grid(sex~species) +
+  labs(title="Penguin Body Mass vs Flipper Length", subtitle="Segmented by Species and Sex",
+       caption="Data collected by Dr. Kristen Gorman")
